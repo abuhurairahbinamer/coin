04f45f970863f700f0e33f21fedf3ff7e353fbbb
please <abuhurairah131@gmail.com>
2023-08-05 13:36:41 +0500

    blogcomplete

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -2,6 +2,7 @@
 const Joi=require("joi")
 const fs=require('fs');
 const Blog=require('../models/blog')
+const Comment=require('../models/comment')
 const {BACKEND_SERVER_PATH}=require('../config/index')
 const BlogDTO=require('../dto/blog')
 const BlogsDetailsDTO=require('../dto/blogs-details')
@@ -105,14 +106,30 @@ if (error){
 }
 const {id}=req.params;
 let blog
-try {
-   blog= await Blog.findOne({_id:id}).populate('author');
+let IdObject={
+  _id:id
+}
+// try {
+//    blog= await Blog.findOne({_id:id}).populate('author');
 
+// } catch (error) {
+
+//   return next (error);
+
+// }
+
+// or
+ // so conslusion is you can pass the object name or you can manually pass object
+try {
+   blog= await Blog.findOne(IdObject).populate('author');
+   console.log("hellow find by id")
 } catch (error) {
 
   return next (error);
 
 }
+
+
 
 const blog_details_Dto=new BlogsDetailsDTO(blog);
 // send response
@@ -123,9 +140,123 @@ return res.status(200).json({blog:blog_details_Dto});
 },
 
 async update(req,res,next){
+// validate
+const updateBlogSchema=Joi.object({
+  title:Joi.string().required(),
+  content:Joi.string().required(),
+  author:Joi.string().regex(mongodbIdPattern).required(),
+  blogId:Joi.string().regex(mongodbIdPattern).required(),
+  photo:Joi.string()
+})
+
+const {error}=updateBlogSchema.validate(req.body);
+if(error){
+  return next(error);
+}
+const {title,content,author,blogId,photo} = req.body;
+
+//delete previous photo
+let blog
+try {
+  
+ blog =await Blog.findOne({_id: blogId});
+
+} catch (error) {
+return next(error);  
+}
+
+if(photo){
+
+  let previousPhoto=blog.photoPath;
+  
+  previousPhoto=previousPhoto.split('/').at(-1);
+// delete photo
+try {
+  fs.unlinkSync(`storage/${previousPhoto}`);
+  
+} catch (error) {
+return next(error);  
+}
+
+
+// adding new photo
+
+  //2.read as buffer
+  const buffer = Buffer.from(
+    photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ""),
+    "base64"
+  );
+ // allot random name
+ const imagePath = `${Date.now()}-${author}.png`;
+
+   // save image locally
+   try {
+    fs.writeFileSync(`storage/${imagePath}`, buffer);
+   } catch (error) {
+    return next(error);
+   }
+
+   try {
+    await Blog.updateOne({_id:blogId},{title,content,photoPath:`${BACKEND_SERVER_PATH}/storage/${imagePath}`})
+   } catch (error) {
+    return next(error)
+   }
+
+
+}
+
+
+else{
+  try {
+    await Blog.updateOne({_id:blogId},{title,content})
+  } catch (error) {
+    return next(error)
+  }
+}
+
+let message="updated successfully";
+return res.status(200).json({message})
+
+
 
 },
-async delete(req,res,next){}
+async delete(req,res,next){
+  // validate id
+  const deleteSchema=Joi.object({
+    id:Joi.string().regex(mongodbIdPattern).required()
+  }) 
+  const {error}=deleteSchema.validate(req.params);
+  if (error){
+    return next(error);
+  }
+
+
+
+  const {id}=req.params;
+  
+
+  try {
+    //deleting image
+
+    const blog =await Blog.findOne({_id: id}) 
+    let previousPhoto=blog.photoPath;
+    previousPhoto=previousPhoto.split('/').at(-1);
+    fs.unlinkSync(`storage/${previousPhoto}`);
+  
+    // delete blog
+     
+  await Blog.deleteOne({_id:id});
+  
+  // delete comments on this blog
+  
+  await Comment.deleteMany({blog:id});
+  } catch (error) {
+    return next(error)
+  }
+  
+  res.status(200).json({message:"deleted sucessfully"})
+}
+
 }
 
 module.exports=blogController;
